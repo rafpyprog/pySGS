dbe3a4ef6bb106c9b03ba7e5ee84148f364e4c6d
rafpyprog <rafael.alves.ribeiro@gmail.com>
2018-02-06 09:58:37 -0200

    1.27.2

--- publish.bat
+++ publish.bat
@@ -1,1 +1,8 @@
-python setup.py sdist upload -r pypi
+echo Publicando versão %2 do pacote
+echo __version__ = '%2' > .\sgs\__version__.py
+git add .
+git commit -m "%1"
+git tag %2
+git push
+git push --tags
+echo python setup.py sdist upload -r pypi
