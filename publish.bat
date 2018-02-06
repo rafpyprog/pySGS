e226843d7909dca246b1517fbb51640758851078
rafpyprog <rafael.alves.ribeiro@gmail.com>
2018-02-06 10:07:40 -0200

    Script de publicação do pacote

--- publish.bat
+++ publish.bat
@@ -1,10 +1,19 @@
+REM Atualiza o repositório no Github e publica o pacote o PyPi
+REM Argumentos:
+REM   * mensagem de commit
+REM   * número da versão
+
 echo OFF
-echo Publicando versao %2 do pacote
-echo ==============================
+
+echo '- Atualizando versão para %2'
 echo __version__ = '%2' > .\sgs\__version__.py
+
+echo '- Atualizando repositório do Github'
 git add .
 git commit -m %1
 git tag %2
 git push
 git push --tags
+
+echo '- Realizando upload para o Pypi'
 python setup.py sdist upload -r pypi
