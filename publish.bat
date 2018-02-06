e0a87636380c94cab6c0974b255ab46d90841cbe
rafpyprog <rafael.alves.ribeiro@gmail.com>
2018-02-06 10:01:50 -0200

    Script de publicação do pacote

--- publish.bat
+++ publish.bat
@@ -1,10 +1,10 @@
 echo OFF
-echo 'Publicando versão %2 do pacote'
-echo '=============================='
+echo Publicando versao %2 do pacote
+echo ==============================
 echo __version__ = '%2' > .\sgs\__version__.py
 git add .
 git commit -m %1
 git tag %2
 git push
 git push --tags
-echo python setup.py sdist upload -r pypi
+python setup.py sdist upload -r pypi
