dc76ef651d4093ec175c3d44e52adcbcde242faf
Rafael Ribeiro <rafael.alves.ribeiro@gmail.com>
2019-12-10 00:26:29 -0300

    Fixes #28 (#29)
    * bump version on setup.py
    
    * add badges to readme
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    * Updates README.rst
    
    *  Fix bug when time series search returned more than one result pages
    
    * Fix bug when time series search returned more than one result pages
    
    * Fix bug when time series search returned more than one result pages
    
    * Fix bug when time series search returned more than one result pages
    
    * Fix bug when time series search returned more than one result pages
    
    * Fix bug when time series search returned more than one result pages
    
    * Fix date parsing when only year is present
    
    * fix date parsing when only year is present
    
    * fix date parsing when only year is present
    
    * fix date parsing when only year is present
    
    * Fix date parsing when only year is present and enforce srtring inputs on date time transformation
    
    * fix date parsing when only year is present
    
    * fix date parsing when only year is present
    
    * fix date parsing when only year is present
    
    * fix date parsing when only year is present
    
    * fixes #28

--- tests/test_ts.py
+++ tests/test_ts.py
@@ -9,3 +9,10 @@ def test_time_serie():
     ts = time_serie(4, "02/01/2018", "31/01/2018")
     assert len(ts) == 20
     assert ts.dtype == np.float
+
+@pytest.mark.ts
+def test_ts_with_null_values():
+    # Issue #28
+    ts = time_serie(21554, start="31/12/1992", end="01/06/2019")
+    data = ts.loc['1994-04-01']    
+    assert np.isnan(data) == True
