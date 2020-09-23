35065b42e5fa24dac2dcecc6125393b3379bb47b
Angelo Salton <gsalton4@hotmail.com>
2020-09-23 13:58:28 -0300

    Added call to BCB Olinda API

--- sgs/api.py
+++ sgs/api.py
@@ -1,3 +1,4 @@
+import datetime
 import functools
 from typing import Union, List, Dict
 
@@ -20,5 +21,37 @@ def get_data(ts_code: int, begin: str, end: str) -> List:
         "/dados?formato=json&dataInicial={}&dataFinal={}"
     )
     request_url = url.format(ts_code, begin, end)
+    response = requests.get(request_url)
+    return response.json()
+
+
+@retry(stop_max_attempt_number=MAX_ATTEMPT_NUMBER)
+@functools.lru_cache(maxsize=LRU_CACHE_SIZE)
+def get_data_olinda(resource: str, begin: str, end: Union[None, str]) -> List:
+    """
+    Requests data frames from the BCB Olinda API in json format.
+    """
+    # resource names
+    names = {
+        'monthly': 'ExpectativaMercadoMensais',
+        'quarterly': 'ExpectativaMercadoTrimestrais',
+        'yearly': 'ExpectativaMercadoAnuais',
+        'infl12m': 'ExpectativasMercadoInflacao12Meses',
+        'monthlytop5': 'ExpectativasMercadoTop5Mensais',
+        'yearlytop5': 'ExpectativasMercadoTop5Anuais',
+        'inst': 'ExpectativasMercadoInstituicoes'
+        }
+
+    # TODO: Olinda API expects ISO dates
+    begin_iso = datetime.datetime.strptime(begin, '%d/%m/%Y').strftime('%Y-%m-%d')
+    end_iso = datetime.datetime.strptime(end, '%d/%m/%Y').strftime('%Y-%m-%d')
+
+    url = (
+        "https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/"
+        "{}?$filter=Data%20ge%20'{}'%20and%20Data%20le%20'{}'&"
+        "$orderby=Data%20asc&$format=json"
+    )
+
+    request_url = url.format(names[resource], begin_iso, end_iso)
     response = requests.get(request_url)
     return response.json()
