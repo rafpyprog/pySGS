9ce0d98ca8260ff572e344ed0700328f259f6974
joao <js.cad@pm.me>
2020-11-03 16:28:09 -0300

    Tests covering strict parameter included

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,3 +10,9 @@ def test_get_data():
     data = api.get_data(4, "02/01/2018", "31/01/2018")
     assert isinstance(data, list)
     assert len(data) == NUMBER_OF_LINES
+    
+@pytest.mark.api
+def test_enforce_integrity():
+    data = api.get_data(20577, "17/08/2019", "18/08/2019", True)
+    assert isinstance(data, list)
+    assert len(data) == 0
