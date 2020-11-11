0160d9486a070c82641fc8d85070f0b59fbbafe9
joao <js.cad@pm.me>
2020-11-11 17:01:35 -0300

    Incluidas novas funcoes de data em common.py

--- sgs/common.py
+++ sgs/common.py
@@ -2,9 +2,9 @@
 Shared functions.
 """
 from datetime import datetime
-import locale
-import re
 from typing import Union
+import locale
+import pandas as pd
 import os
 
 
@@ -12,35 +12,67 @@ LRU_CACHE_SIZE = 32
 MAX_ATTEMPT_NUMBER = 5
 
 
-def to_datetime(date_string: str, language: str) -> Union[datetime, str]:
-    """ Converts a date string to a datetime object """
-    locales = {"pt": "pt_BR.utf-8", "en": "en_US.utf-8"}
+def to_datetime(date_string: str, language: str) -> datetime:
 
     """ correct problem with locale in Windows platform """
-    if os.name == 'nt':
+    if os.name == "nt":
         locales = {"pt": "Portuguese_Brazil.1252", "en": "Portuguese_Brazil.1252"}
-    
+    else:
+        locales = {"pt": "pt_BR.utf-8", "en": "en_US.utf-8"}
+
     locale.setlocale(locale.LC_TIME, locales[language])
 
     dd_mm_aaaa = "%d/%m/%Y"
     mmm_aaaa = "%b/%Y"
+    aaaa = "%Y"
 
-    formats = [dd_mm_aaaa, mmm_aaaa]
+    formats = [dd_mm_aaaa, mmm_aaaa, aaaa]
 
     for fmt in formats:
         try:
             date = datetime.strptime(date_string, fmt)
+            if fmt == aaaa:
+                date = date.replace(day=31, month=12)
             break
         except ValueError:
             continue
     else:
-        yyyy = "[0-9]{4}"
-        if re.match(yyyy, date_string):
-            year = int(date_string)
-            month = 12
-            day = 31
-            date = datetime(year, month, day)
-        else:
-            return date_string  # returns original value if cant parse
-
+        raise ValueError
     return date
+
+
+def to_datetime_string(date_string: str, language: str) -> str:
+
+    try:
+        date = to_datetime(date_string, language).strftime("%Y-%m-%d")
+    except ValueError:
+        date = date_string
+    return date
+
+
+def apply_strict_range(
+    data: Union[pd.DataFrame, pd.Series], start: str, end: str
+) -> Union[pd.DataFrame, pd.Series]:
+
+    if isinstance(data, pd.Series):
+        codes = [data.name]
+    else:
+        codes = data.columns
+
+    try:
+        enforce_start = to_datetime(start, "pt")
+        enforce_end = to_datetime(end, "pt")
+        strict_data = data[data.index.to_series().between(enforce_start, enforce_end)]
+    except ValueError:
+        strict_data = data.drop(data.index)
+        for ts_code in codes:
+            print(
+                "ERROR: Serie %s - use 'DD/MM/YYYY' format for date strings." % ts_code
+            )
+    if not data.empty and strict_data.empty:
+        for ts_code in codes:
+            print(
+                "WARNING: Serie %s - There is no data for the requested period."
+                % ts_code
+            )
+    return strict_data
