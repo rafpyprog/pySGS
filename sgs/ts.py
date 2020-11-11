2c5e8f6a1b4bc42c3c33b71d4ba0ec89586bf90c
joao <js.cad@pm.me>
2020-11-11 17:09:00 -0300

    Alterado modo strict da funcao time_serie

--- sgs/ts.py
+++ sgs/ts.py
@@ -1,14 +1,12 @@
 """
 Time Serie manipulation
 """
-from typing import Dict, List, Optional
 
 import numpy as np
 import pandas as pd
 
 from . import api
-from . import search
-from .common import to_datetime
+from .common import to_datetime, apply_strict_range
 
 
 def time_serie(ts_code: int, start: str, end: str, strict: bool = False) -> pd.Series:
@@ -34,18 +32,19 @@ def time_serie(ts_code: int, start: str, end: str, strict: bool = False) -> pd.S
         2018-01-05    0.026444
         2018-01-08    0.026444
     """
-    
-    if strict:
-        ts_data = api.get_data_with_strict_range(ts_code, start, end)
-    else:
-        ts_data = api.get_data(ts_code, start, end)
-        
+
     values = []
     index = []
-    for i in ts_data:
+    for i in api.get_data(ts_code, start, end):
         values.append(i["valor"])
         index.append(to_datetime(i["data"], "pt"))
 
     # Transform empty strings in null values
     values = [np.nan if value == "" else value for value in values]
-    return pd.Series(values, index, name=ts_code, dtype=np.float)
+
+    ts = pd.Series(values, index, name=ts_code, dtype=np.float)
+
+    if strict:
+        ts = apply_strict_range(ts, start, end)
+
+    return ts
