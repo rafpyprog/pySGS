a8b1aa4c08e99fcb731bcd88734175ae3a5e3d3b
joao <js.cad@pm.me>
2021-01-05 20:02:57 -0300

    strict incuded in pytest

--- sgs/strict.py
+++ sgs/strict.py
@@ -3,9 +3,7 @@ from typing import Union
 from .common import to_datetime, get_series_codes
 
 
-def apply_strict_range(
-    data: Union[pd.DataFrame, pd.Series], start: str, end: str
-) -> Union[pd.DataFrame, pd.Series]:
+def apply_strict_range(data: Union[pd.DataFrame, pd.Series], start: str, end: str) -> Union[pd.DataFrame, pd.Series]:
 
     """
     SGS API default behaviour returns the last stored value when selected date range have no data.
@@ -24,13 +22,13 @@ def apply_strict_range(
         enforce_start = to_datetime(start, "pt")
         enforce_end = to_datetime(end, "pt")
         strict_data = data[data.index.to_series().between(enforce_start, enforce_end)]
+        if strict_data.empty or data.empty:
+            RuntimeError
     except ValueError:
         strict_data = data.drop(data.index)
-        for ts_code in get_series_codes(data):
-            print("ERROR: Serie %s - use 'DD/MM/YYYY' format for date strings." % ts_code)
-   
-    if not data.empty and strict_data.empty:
-        for ts_code in get_series_codes(data):
-            print("WARNING: Serie %s - There is no data for the requested period." % ts_code)
+        print("ERROR: Please, use 'DD/MM/YYYY' format for date strings.")
+    except RuntimeError:
+        series = ','.join(str(code) for code in get_series_codes(data))
+        print("WARNING: Serie(s) %s - There is no data for the requested period." % series)
 
     return strict_data
